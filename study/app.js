5a2c8101e96d2c61a9a4de2466d5d0ea05754605
JungYoungSick <junfas1216@gmail.com>
2023-11-21 16:47:49 +0900

    문제 5까지 작업 완료

---
+++ study/app.js
@@ -0,0 +1,25 @@
+console.log('Hello, World!');
+//! -----------------------
+
+let ten = 10;
+//! -----------------------
+
+const Hello = "Hello,";
+const JavaScript = " Javascript!";
+const string = Hello + JavaScript
+console.log(string)
+//! -----------------------
+
+function plus() {
+  a = 5
+  b = 10
+  return a + b
+}
+console.log(plus())
+//! -----------------------
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i)
+}
+//! -----------------------
+//! -----------------------
